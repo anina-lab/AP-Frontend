99f35b600b0778419f317849a1854b43701099f2
Anina L <anina.learn@gmail.com>
2022-10-29 18:09:05 -0300

    Initial commit

---
+++ src/app/service/acerca.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AcercaService {
+  URL = 'http://localhost:8080/acerca/';
+  
+  constructor(private httpClient: HttpClient) { }
+
+}
